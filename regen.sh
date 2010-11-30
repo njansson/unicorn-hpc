b53149e1296287792c735dae61ec07286906afd3
Niclas Jansson <njansson@csc.kth.se>
2010-11-30 15:28:53 +0100

    Regen script

---
+++ regen.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+echo "Updating configuration..."
+echo "Running libtoolize"
+libtoolize -i
+
+echo "Running aclocal"
+if which aclocal > /dev/null 2>&1; then
+  aclocal -I m4 --install
+else
+  echo "No aclocal found on your system"
+  exit 1
+fi
+
+echo "Running autoconf"
+autoconf
+echo "Running automake"
+automake -a
+
+echo "Deleting autom4te.cache directory"
+rm -r autom4te.cache
+
