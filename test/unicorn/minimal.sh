2e751995506d2ce7427250ab198eb50187cf6b87
Niclas Jansson <njansson@csc.kth.se>
2011-04-21 12:55:28 +0200

    Added basic unicorn testing

--- test/unicorn/minimal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo 'Running minimal solver test'
-rm -fr iter*
-mpirun -np 1 ./minimal -p parameters -m usquare.xml
-rm -fr iter*
-mpirun -np 4 ./minimal -p parameters -m usquare.xml
-rm -fr iter*
-mpirun -np 9 ./minimal -p parameters -m usquare.xml
