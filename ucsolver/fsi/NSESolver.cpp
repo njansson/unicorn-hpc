de758bc258474e57fff6eb8788c41f8ee72afc40
Johan Jansson <jjan@csc.kth.se>
2011-08-04 12:39:58 +0200

    MeshBC doesn't hold..

--- ucsolver/fsi/NSESolver.cpp
+++ ucsolver/fsi/NSESolver.cpp
@@ -669,26 +669,26 @@ void NSESolver::smoothMesh()
     
   }
 
-  if(false && smooth_counter == 5)
+  if(true || smooth_counter == 5)
   {
     int ode_max_it = dolfin_get("ODE maximum iterations");
     real ode_tol_save = dolfin_get("ODE discrete tolerance");
     dolfin_set("ODE maximum iterations", 3);
     if((mqual->mu_min < 0.4 * mu_bar))
     {
-      dolfin_set("Smoother max time steps", 2);
+      dolfin_set("Smoother max time steps", 20);
       smoother->smooth(smoothed, solid_vertices, h0);
       did_smoothing = true;
     }
     else if(mqual->mu_min < 0.5 * mu_bar)
     {
-      dolfin_set("Smoother max time steps", 2);
+      dolfin_set("Smoother max time steps", 20);
       smoother->smooth(smoothed, solid_vertices, h0);
       did_smoothing = true;
     }
     else
     {
-      dolfin_set("Smoother max time steps", 2);
+      dolfin_set("Smoother max time steps", 20);
       smoother->smooth(smoothed, solid_vertices, h0);
       did_smoothing = true;
     }
