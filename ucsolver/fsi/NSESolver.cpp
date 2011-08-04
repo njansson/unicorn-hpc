cfe8d5ca72578b6d280b64fd22b9e104dab4b05f
Johan Jansson <jjan@csc.kth.se>
2011-08-04 21:49:36 +0200

    Works ok with few smoothing steps as well. Smoother fixed-point eventually diverges though, need to take more conservative smoother time steps.

--- ucsolver/fsi/NSESolver.cpp
+++ ucsolver/fsi/NSESolver.cpp
@@ -676,19 +676,19 @@ void NSESolver::smoothMesh()
     dolfin_set("ODE maximum iterations", 3);
     if((mqual->mu_min < 0.4 * mu_bar) || t < 30 * k)
     {
-      dolfin_set("Smoother max time steps", 20);
+      dolfin_set("Smoother max time steps", 2);
       smoother->smooth(smoothed, solid_vertices, h0);
       did_smoothing = true;
     }
     else if(mqual->mu_min < 0.5 * mu_bar)
     {
-      dolfin_set("Smoother max time steps", 20);
+      dolfin_set("Smoother max time steps", 2);
       smoother->smooth(smoothed, solid_vertices, h0);
       did_smoothing = true;
     }
     else
     {
-      dolfin_set("Smoother max time steps", 20);
+      dolfin_set("Smoother max time steps", 2);
       smoother->smooth(smoothed, solid_vertices, h0);
       did_smoothing = true;
     }
