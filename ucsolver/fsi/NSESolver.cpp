0f6796318b0d48fb32acd471a59c89fd1d732575
Johan Jansson <jjan@csc.kth.se>
2011-08-06 01:44:26 +0200

    Isolate noisy theta to elastic smoothing.

--- ucsolver/fsi/NSESolver.cpp
+++ ucsolver/fsi/NSESolver.cpp
@@ -609,7 +609,7 @@ void NSESolver::smoothMesh()
 
   bool did_smoothing = true;
 
-  if(false && smooth_counter < 5)
+  if(true || smooth_counter < 5)
   {
     bool reset_lsmoother = false;
     if(t == 0.0)
@@ -694,7 +694,7 @@ void NSESolver::smoothMesh()
     
   }
 
-  if(true || smooth_counter == 5)
+  if(false && smooth_counter == 5)
   {
     int ode_max_it = dolfin_get("ODE maximum iterations");
     real ode_tol_save = dolfin_get("ODE discrete tolerance");
