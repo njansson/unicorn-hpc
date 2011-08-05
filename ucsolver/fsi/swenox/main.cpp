889efb238cd90ba98901df666d69a5d959802f22
Johan Jansson <jjan@csc.kth.se>
2011-08-05 04:10:06 +0200

    Smoothing is broken again.

--- ucsolver/fsi/swenox/main.cpp
+++ ucsolver/fsi/swenox/main.cpp
@@ -517,7 +517,7 @@ int main(int argc, char* argv[])
 
   unicorn_init(argc, argv, mesh, chkp, w_limit, iter, structure_mesh);
 
-  //transform(mesh);
+  transform(mesh);
 
 //   mesh.refine();
 //   mesh.refine();
