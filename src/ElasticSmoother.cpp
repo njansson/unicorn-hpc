535ce8443614474f60f2dd2c021d90935f82a618
Niclas Jansson <njansson@csc.kth.se>
2011-11-01 22:26:59 +0100

    Minor fixes for Solaris

--- src/ElasticSmoother.cpp
+++ src/ElasticSmoother.cpp
@@ -240,7 +240,7 @@ void ElasticSmoother::submesh(Mesh& mesh, Mesh& sub,
 			      MeshFunction<int>& old2new_cell)
 {
   
-  dolfin_debug("Entering create submesh");
+  //dolfin_debug("Entering create submesh");
 
   old2new_vertex.init(mesh, 0);
   old2new_cell.init(mesh, mesh.topology().dim());
@@ -367,7 +367,7 @@ void ElasticSmoother::submesh(Mesh& mesh, Mesh& sub,
   sub.distdata().invalid_numbering();
   sub.renumber();
 
-  dolfin_debug("submesh created"); 
+  //  dolfin_debug("submesh created"); 
   //  File submesh("submesh.pvd");
   //  submesh << sub;
   
